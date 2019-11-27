af9d91e7fa386cf3da627961a5e631336a9f1d2a
luyang <1291166451@qq.com>
2019-11-27 21:21:22 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="app-container">
-
+   
     <!-- 顶部 Header 区域 -->
     <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
-
+<h1>123</h1>
 
     <!-- 中间的 路由 router-view 区域 -->
-		<transition>
-			<router-view></router-view>
-		</transition>
-
+	
 
     <!-- 底部 Tabbar 区域 -->
-    <nav class="mui-bar mui-bar-tab">
+     <nav class="mui-bar mui-bar-tab">
 			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
@@ -32,33 +29,17 @@
 				<span class="mui-tab-label">搜索</span>
 			</router-link>
 		</nav>
-
-  </div>
+   </div>
 </template>
 
 <script>
+
 </script>
 
 
 <style lang="scss" scoped>
-.app-container {
+  .app-container {
   padding-top: 40px;
   overflow-x: hidden;
-}
-
-.v-enter {
-  opacity: 0;
-  transform: translateX(100%);
-}
-
-.v-leave-to {
-  opacity: 0;
-  transform: translateX(-100%);
-  position: absolute;
-}
-
-.v-enter-active,
-.v-leave-active {
-  transition: all 0.5s ease;
 }
 </style>
